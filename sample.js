1c355a6f1b7789acd19d120ee20152fa877a7286
laefy13 <karloosss13@gmail.com>
2023-12-05 15:43:43 +0800

    waiting for the api

--- sample.js
+++ sample.js
@@ -1,20 +1,19 @@
 import { client } from "https://cdn.jsdelivr.net/npm/@gradio/client@0.8.2/dist/index.min.js";
 
-const response_0 = await fetch(
-  "https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png"
-);
+const response_0 = await fetch("https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png");
 const exampleImage = await response_0.blob();
+						
+const app = await client("http://127.0.0.1:7860/");
 
-const app = await client(
-  "https://laefy-tomato.hf.space/--replicas/jvqrg/"
-);
-const result = await app.predict("/main", [
-  exampleImage, // blob in 'Input Image' Image component
-  true, // boolean  in 'Segment' Checkbox component
-]);
 
+const result = await app.predict("/main_1", [		
+				"marked_clean.png", // string  in 'image_path' Textbox component		
+				true, // boolean  in 'Segment' Checkbox component		
+				true, // boolean  in 'Segment' Checkbox component
+	]);
+
+
+  
 console.log(result.data);
 
-document
-  .getElementById("generate-button")
-  .addEventListener("click", predict);
+
--- script.js
+++ script.js
@@ -1,82 +1,92 @@
-import { client } from "https://cdn.jsdelivr.net/npm/@gradio/client@0.1.4/dist/index.min.js";
+import { client } from "https://cdn.jsdelivr.net/npm/@gradio/client@0.8.2/dist/index.min.js";
 // Function to handle predictions
+
+
+function uploadImage(image_) {
+  return new Promise((resolve, reject) => {
+    const input = document.getElementById('imageInput');
+    const file = input.files[0];
+
+    const formData = new FormData();
+    formData.append('image', file);
+
+    let imageUrl; 
+
+    fetch('https://api.imgbb.com/1/upload?expiration=60&key=7cbbdced6035920192277675516a2f49', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        imageUrl = data.data.url;
+        resolve(imageUrl);
+    })
+    .catch(error => {
+        console.error('Error uploading image:', error);
+        reject(error);
+    });
+  });
+}
+
 async function predict() {
   const imageInput =
     document.getElementById("imageInput");
   const segmentCheckbox =
     document.getElementById("segment_in");
-
-  // Get the selected image file
+  
   const file = imageInput.files[0];
+  console.log(file)
+  const url = await uploadImage(file);
   if (!file) {
     alert("Please select an image");
     return;
   }
 
-  // Convert the image file to a Blob
-  const imageData = await readFileAsBlob(file);
-  console.log(imageData);
-
-  // Gradio client setup
   const app = await client(
-    "https://laefy-tomato.hf.space/--replicas/jvqrg/"
+    "http://127.0.0.1:7860/"
   );
 
-  // Perform prediction
-  const result = await app.predict("/main", [
-    imageData, // Image blob
-    segmentCheckbox.checked, // Checkbox value
+  const result = await app.predict("/main_1", [
+    url, 
+    segmentCheckbox.checked, 
+    true, 
   ]);
-
-  // Display the result
-  document.getElementById(
-    "result"
-  ).innerHTML = `<pre>${JSON.stringify(
-    result.data,
-    null,
-    2
-  )}</pre>`;
+  
+  const confidence = document.getElementById('confidence_in');
+  const classification = document.getElementById('classification_in');
+  const previewImage = document.getElementById('previewImage');
+  const outputImage = document.getElementById('outputImage');
+  console.log(result.data);
+  console.log(result.data[2].url);
+  classification.value = result.data[0]
+  confidence.value = result.data[1]
+  previewImage.src = result.data[3].url
+  outputImage.src = result.data[2].url
 }
 
-// Event listener for the prediction button
 document
   .getElementById("generate-button")
   .addEventListener("click", predict);
 
-// Function to convert file to Blob
-async function readFileAsBlob(file) {
-  return new Promise((resolve) => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const dataUrl = event.target.result;
-      const byteString = atob(
-        dataUrl.split(",")[1]
-      );
-      const mimeString = dataUrl
-        .split(",")[0]
-        .split(":")[1]
-        .split(";")[0];
-      const arrayBuffer = new ArrayBuffer(
-        byteString.length
-      );
-      const intArray = new Uint8Array(
-        arrayBuffer
-      );
 
-      for (
-        let i = 0;
-        i < byteString.length;
-        i++
-      ) {
-        intArray[i] = byteString.charCodeAt(i);
+document.addEventListener('DOMContentLoaded', function () {
+  const imageInput = document.getElementById('imageInput');
+  const previewImage = document.getElementById('previewImage');
+
+  imageInput.addEventListener('change', function () {
+      const file = this.files[0];
+
+      if (file) {
+          const reader = new FileReader();
+
+          reader.onload = function (e) {
+              previewImage.src = e.target.result;
+          };
+
+          reader.readAsDataURL(file);
+      } else {
+          // If no file is selected, reset the image to the default
+          previewImage.src = 'upload.png';
       }
-
-      const blob = new Blob([intArray], {
-        type: mimeString,
-      });
-      resolve(blob);
-    };
-
-    reader.readAsDataURL(file);
   });
-}
+});
